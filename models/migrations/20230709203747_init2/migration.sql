cb6f99fd562051b04353d5bd559b986981a9042a
ced_stark08 <cedricngangue08@gmail.com>
2023-07-09 22:50:05 +0200

    reset migration data4

--- models/migrations/20230709203747_init2/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `isFrequency` on the `InterventionLogiciel` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "InterventionLogiciel" DROP COLUMN "isFrequency",
-ADD COLUMN     "isBegin" BOOLEAN NOT NULL DEFAULT false;
